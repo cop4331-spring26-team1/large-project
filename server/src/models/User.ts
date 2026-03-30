f2b216e06d80fb4cd3d7b967c4a6667c34cbf88a
Kyle Russell <ky948225@ucf.edu>
2026-03-30 09:19:57 -0400

    Added User and Listing DB Schema.

---
+++ server/src/models/User.ts
@@ -0,0 +1,17 @@
+import mongoose, {Document, Schema} from 'mongoose'
+
+export interface IUser extends Document {
+    name: string
+    email: string
+    hashedPassword: string
+    role: 'user' | 'admin'
+    dateCreated: Date
+}
+
+const UserSchema = new Schema<IUser>({
+    name: {type: String, required: true, trim: true},
+    email: {type: String, required: true, unique: true, lowercase: true},
+    hashedPassword: {type: String, required: true},
+    role: {type: String, enum: ['user', 'admin'], default: 'user'},
+    dateCreated: {type: Date, default: Date.now}
+})
